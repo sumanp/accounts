91b70af03c5399f2eb7b93d2b2569d5b6b4b2afd
sumanp <sumanpuri55@gmail.com>
2024-02-07 00:51:31 +0530

    Bug fix

--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,5 @@
 require 'bcrypt'
+require_relative '../helpers/authentication_helpers'
 
 class User < ActiveRecord::Base
   has_secure_password
@@ -6,10 +7,4 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'is not a valid email address'
   validates :secret_key, uniqueness: true, allow_nil: true
-
-  def enable_otp
-    otp_secret = AuthenticationHelpers.generate_otp_secret
-    self.update(secret_key: otp_secret, two_factor_enabled: true)
-    otp_secret
-  end
 end
